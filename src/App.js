9181358226787f60569ff8737df9e19847536d84
amex23 <randelb2022@gmail.com>
2022-12-07 23:54:13 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header bg-red-400">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>sdsdsdsd</p>
+        <div className="bg-red-400">
+          <h1 className="text-3xl font-bold underline">Hello world!</h1>
+          <button className="btn btn-outline btn-success">Success</button>
+        </div>
       </header>
     </div>
   );
