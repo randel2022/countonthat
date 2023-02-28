54527d500ae40f72b32ddf6433de602fb80da0f0
randel2022 <yumimadigal13@gmail.com>
2023-02-28 15:18:59 +0800

    alpha characters in contact fix

--- src/components/Calculate.js
+++ src/components/Calculate.js
@@ -450,7 +450,7 @@ const InputNames = ({
               placeholder="Contact Number"
               name="contact"
               type="number"
-              onKeyDown={(evt) => evt.key.match(/^[a-zA-Z]$/) && evt.preventDefault()}
+              onKeyDown={(evt) => !(evt.key.match(/^[0-9]$/) || evt.key('Backspace')) && evt.preventDefault()}
               value={item.contact}
               onChange={(e) =>
                 onChangeInputValue(
